04e5843750250c20ba55c450408022cc784c563c
huqiang1115 <981584883@qq.com>
2019-04-29 14:32:40 +0800

    create project

---
+++ src/main/java/com/bj/common/utils/RabbitmqUtils.java
@@ -0,0 +1,9 @@
+package com.bj.common.utils;
+
+public class RabbitmqUtils {
+	//定义多个常量值
+	public static final String type="direct";
+	public static final String EX="seckillDEX";
+	public static final String queue="seckillQ";
+	public static final String routingKey="seckill";
+}
